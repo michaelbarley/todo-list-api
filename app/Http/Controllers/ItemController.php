4036049d69abc390c418bff36e7c98398b0687ec
Michael Barley <mr.michaelbarley@gmail.com>
2023-06-12 14:37:15 +0100

    Initial commit

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use App\Http\Requests\StoreItemRequest;
+use App\Http\Requests\UpdateItemRequest;
+use App\Http\Resources\ItemResource;
+use Illuminate\Http\Response;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ItemResource::collection(Item::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreItemRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreItemRequest $request)
+    {
+        $item = Item::create($request->validated());
+    
+        return (new ItemResource($item))->response()->setStatusCode(Response::HTTP_CREATED);
+    }
+    
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Item $item)
+    {
+        return new ItemResource($item);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateItemRequest  $request
+     * @param  \App\Models\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateItemRequest $request, Item $item)
+    {
+        $item->update($request->validated());
+    
+        return (new ItemResource($item))->response()->setStatusCode(Response::HTTP_OK);
+    }
+    
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Item $item)
+    {
+        $item->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+}
